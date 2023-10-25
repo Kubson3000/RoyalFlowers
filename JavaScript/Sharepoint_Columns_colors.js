808a6ebeddba40f2207374a2a208250753d1bb76
Kubson3000 <86226182+Kubson3000@users.noreply.github.com>
2023-10-25 08:31:00 +0200

    Update Sharepoint_Columns_colors.js

--- JavaScript/Sharepoint_Columns_colors.js
+++ JavaScript/Sharepoint_Columns_colors.js
@@ -11,16 +11,16 @@ function changeColorBasedOnDate(element, dateStr, key) {
     if (key === 'CFRdo') {
         if (diffDays <= 14 && inputDate >= currentDate) {
             element.style.backgroundColor = '#e977be';
-            element.style.color = "white";
         } else if (inputDate < currentDate) {
             element.style.backgroundColor = '#db2e74';
+            element.style.color = "white";
         }
     } else if (key === 'A1do') {
         if (diffDays <= 14 && inputDate >= currentDate) {
             element.style.backgroundColor = '#b4a7d6';
-            element.style.color = "white";
         } else if (inputDate < currentDate) {
             element.style.backgroundColor = '#674ea7';
+            element.style.color = "white";
         }
     }
 }
